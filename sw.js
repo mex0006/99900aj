a16bc77f9be88f4646509c7d07c061fa8cd5a981
mex0006 <mertkarakus@protonmail.com>
2025-08-11 07:20:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
-const CACHE_NAME='ajanda-v9-3-fix1';
-const ASSETS=['./','./index.html','./css/main.css','./js/app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME='ajanda-v9-4-full';
+const ASSETS=[
+  './',
+  './index.html',
+  './css/main.css?v=9.4',
+  './js/app.js?v=9.4',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install',e=>{e.waitUntil(
+  caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))
+)});
+self.addEventListener('activate',e=>{e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)));
+});
